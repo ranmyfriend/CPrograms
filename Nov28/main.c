bc367dfe16ccccec976fd7f8f5f938c237e9829b
ranjit <ranjithkumar2010a@live.com>
2018-11-28 12:20:03 +0530

    Initial Commit

---
+++ Nov28/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Nov28
+//
+//  Created by Ranjith Kumar on 11/28/18.
+//  Copyright © 2018 DrawRect. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
